591b7cff325848ccb3e1076e7488ce1a508b8436
Volkrend <jecavf14@gmail.com>
2026-06-23 11:48:27 -0600

    Add files via upload

--- Proyecto-Grafos/Arco.h
+++ Proyecto-Grafos/Arco.h
@@ -1,5 +1,50 @@
-#pragma once
+//Representa un arco (arista) de un grafo no dirigido
+//Conecta dos nodos y almacena la distancia entre ellos.
+
 class Arco {
+private:
+    int nodo1;
+    int nodo2;
+    int distancia;
 
+
+public:
+    // Constructor vacío
+    Arco() {
+        nodo1 = 0;
+        nodo2 = 0;
+        distancia = 0;
+    }
+
+public:
+
+    Arco(int nodo1, int nodo2, int peso) {
+        this->nodo1 = nodo1;
+        this->nodo2 = nodo2;
+        this->distancia = distancia;
+    }
+
+    int getNodo1() const {
+        return nodo1;
+    }
+
+    int getNodo2() const {
+        return nodo2;
+    }
+
+    int getPeso() const {
+        return distancia;
+    }
+
+    void setNodo1(int nodo1) {
+        this->nodo1 = nodo1;
+    }
+
+    void setNodo2(int nodo2) {
+        this->nodo2 = nodo2;
+    }
+
+    void setPeso(int peso) {
+        this->distancia = distancia;
+    }
 };
-
