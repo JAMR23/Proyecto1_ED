ea3f25cce6222b703fc4fd4882856e4ad88ea6c2
Indigo Sánchez <indigonikkis@gmail.com>
2026-06-22 18:07:34 -0600

    Se añade el Nodo

---
+++ Proyecto-Grafos/Nodo.h
@@ -0,0 +1,36 @@
+#pragma once
+
+class Nodo {
+private: 
+	int id;		//identificador
+	float x;
+	float y;
+
+public:
+	Nodo(int id, float x, float y) {
+		this->id = id;
+		this->x = x;
+		this->y = y;
+	}
+
+	int getId() {
+		return id;
+	}
+
+	float getX() {
+		return x;
+	}
+
+	float getY() {
+		return y;
+	}
+
+	float setX(float x) {
+		this->x = x;
+	}
+
+	float setY(float y) {
+		this->y = y;
+	}
+};
+
